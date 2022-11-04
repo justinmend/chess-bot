40eff28d74c7c4db20df5707134949bf3d2d88de
justinmend <justin.mendiguarin@gmail.com>
2022-11-04 08:59:42 -0700

    Cleanup socket.io examples in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,7 @@ let endPoint = "http://localhost:5000";
 let socket = io.connect(`${endPoint}`);
 
 export default function App() {
-  // Notice the paired variables in the left hand bracket.
-  // The first variable is like a property where you can set a value to it.
-  // The second variable is the setter method name that you will use to
-  // set the value of the first variable.
-  // The value in the useState() is your initial default value.
-  const [messages, setMessages] = useState(["Hello And Welcome"]);
-  const [message, setMessage] = useState("");
   const [connectionStatus, setConnectionStatus] = useState("")
-
-  // This method gets autocalled whenever the messages list length changes.
-  useEffect(() => {
-    getMessages();
-  }, [messages.length]);
 
   useEffect(() => {
     getConnectionStatus();
@@ -29,33 +17,6 @@ export default function App() {
       setConnectionStatus(msg.data);
     })
   }
-  
-  // This is called by the useEffect method whenever the messages list length changes also.
-  // This appends the message sent from the server side to the messages list using
-  // setMessages to change the state.
-  const getMessages = () => {
-    socket.on("message", msg => {
-      setMessages([...messages, msg]);
-    })
-  }
-
-  // Whenever there is a change in the text input field,
-  // this sets that value as the current "message".
-  const onChange = e => {
-    setMessage(e.target.value);
-  };
-
-  const onClick = () => {
-    if (message !== "") {
-      // When send message button is clicked, this emits the message to
-      // our server side.
-      socket.emit("message", message);
-      // This clears the message field once the message is sent using the client.
-      setMessage("");
-    } else {
-      alert("Please Add A Message");
-    }
-  };
 
   return (
     <div className="App">
@@ -63,14 +24,8 @@ export default function App() {
         <Chessboard/>
       </div>
       <div>
-        {messages.length > 0 && 
-          messages.map((msg, index) => (
-            <p key={index}>{msg}</p>
-        ))}
-        <input value={message} name="message" onChange={e => onChange(e)} />
-        <button onClick={() => onClick()}>Send Message</button>
         <div>
-          <p>Status: {connectionStatus}</p>
+          <p>Connection status to flask server: {connectionStatus}</p>
         </div>
       </div>
     </div>
