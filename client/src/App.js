9638eb4a7ddade8815fb512e6cefcce6fe849288
justinmend <justin.mendiguarin@gmail.com>
2022-11-05 17:38:31 -0700

    Comment out flask. Don't need to connect to backend yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,33 @@
 import Chessboard from "./components/Chessboard.js";
 import './App.css';
 import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
-let endPoint = "http://localhost:5000";
-let socket = io.connect(`${endPoint}`);
+// import io from "socket.io-client";
+// let endPoint = "http://localhost:5000";
+// let socket = io.connect(`${endPoint}`);
 
 export default function App() {
-  const [connectionStatus, setConnectionStatus] = useState("")
+  // const [connectionStatus, setConnectionStatus] = useState("")
 
-  useEffect(() => {
-    getConnectionStatus();
-  }, [connectionStatus]);
+  // useEffect(() => {
+  //   getConnectionStatus();
+  // }, [connectionStatus]);
 
-  const getConnectionStatus = () => {
-    socket.on("my response", msg => {
-      setConnectionStatus(msg.data);
-    })
-  }
+  // const getConnectionStatus = () => {
+  //   socket.on("my response", msg => {
+  //     setConnectionStatus(msg.data);
+  //   })
+  // }
 
   return (
     <div className="App">
       <div id="app">
         <Chessboard/>
       </div>
-      <div>
+      {/* <div>
         <div>
           <p>Connection status to flask server: {connectionStatus}</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
